b8f077db6d3ba3cbb8e1e99927321df6e2888f7a
Berlyn Araniva <araniva_berlyn_ale01@student.smc.edu>
2026-07-13 17:52:40 -0700

    added weekly activity data and predictions

---
+++ activityTracker.js
@@ -0,0 +1,65 @@
+// My weekly activity data
+const myWeek = [
+  {
+    day: "Monday",
+    activity: "Drawing",
+    category: "creative",
+    hoursSpent: 2,
+    enjoyment: 9,
+    timeOfDay: "evening"
+  },
+  {
+    day: "Tuesday",
+    activity: "Gym workout",
+    category: "physical",
+    hoursSpent: 1.5,
+    enjoyment: 8,
+    timeOfDay: "morning"
+  },
+  {
+    day: "Wednesday",
+    activity: "Coffee with friends",
+    category: "social",
+    hoursSpent: 2,
+    enjoyment: 7,
+    timeOfDay: "afternoon"
+  },
+  {
+    day: "Thursday",
+    activity: "Painting",
+    category: "creative",
+    hoursSpent: 1,
+    enjoyment: 10,
+    timeOfDay: "evening"
+  },
+  {
+    day: "Friday",
+    activity: "Walking outside",
+    category: "physical",
+    hoursSpent: 1.5,
+    enjoyment: 8,
+    timeOfDay: "afternoon"
+  },
+  {
+    day: "Saturday",
+    activity: "Cooking",
+    category: "creative",
+    hoursSpent: 1,
+    enjoyment: 9,
+    timeOfDay: "evening"
+  },
+  {
+    day: "Sunday",
+    activity: "Watching movies",
+    category: "social",
+    hoursSpent: 3,
+    enjoyment: 6,
+    timeOfDay: "night"
+  }
+];
+
+
+// Predictions:
+// 1. I predict creative activities will have the highest enjoyment scores.
+// 2. I predict creative will be the most common category.
+// 3. I think evening activities may have higher enjoyment because I have more free time.
